a1ad7ae1378a4af55276fcb380aea215d4208dbd
Jon Cram <jon@webignition.net>
2013-07-19 11:17:01 +0100

    Refactor; generalise approach to indentifying parameters in normalisable error messages

--- tests/webignition/HtmlValidationErrorNormaliser/NormaliseTest/GetNormalFormTest.php
+++ tests/webignition/HtmlValidationErrorNormaliser/NormaliseTest/GetNormalFormTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace webignition\HtmlValidationErrorNormaliser\Tests\webignition\HtmlValidationErrorNormaliser\NormalisedSingleParameterErrorType;
+namespace webignition\HtmlValidationErrorNormaliser\Tests\webignition\HtmlValidationErrorNormaliser\NormaliseTest;
 
 use webignition\HtmlValidationErrorNormaliser\Tests\webignition\HtmlValidationErrorNormaliser\BaseTest;
 
@@ -11,8 +11,7 @@ class GetNormalFormTest extends BaseTest {
             'general entity "t" not defined and no default entity',
             'general entity "%0" not defined and no default entity'
         );     
-    }
-    
+    }    
     
     public function testUnknownDeclarationType() {
         $this->normalFormTest(
@@ -48,4 +47,25 @@ class GetNormalFormTest extends BaseTest {
             'end tag for "%0" which is not finished'
         );     
     }
+    
+    public function testDocumentTypeDoesnotAllowElementHereMissingOneOfTwoParameters() {
+        $htmlErrorString = 'document type does not allow element "script" here; missing one of "dt", "dd" start-tag';
+        $expectedNormalForm = 'document type does not allow element "%0" here; missing one of "%1", "%2" start-tag';
+        
+        $this->normalFormTest($htmlErrorString, $expectedNormalForm);
+    }
+    
+    public function testDocumentTypeDoesnotAllowElementHereMissingOneOfThreeParameters() {
+        $htmlErrorString = 'document type does not allow element "DIV" here; missing one of "OBJECT", "MAP", "BUTTON" start-tag';
+        $expectedNormalForm = 'document type does not allow element "%0" here; missing one of "%1", "%2", "%3" start-tag';        
+        
+        $this->normalFormTest($htmlErrorString, $expectedNormalForm);
+    }
+    
+    public function testDocumentTypeDoesnotAllowElementHereMissingOneOfManyParameters() {
+        $htmlErrorString = 'document type does not allow element "img" here; missing one of "p", "h1", "h2", "h3", "h4", "h5", "h6", "div", "address", "fieldset", "ins", "del" start-tag';
+        $expectedNormalForm = 'document type does not allow element "%0" here; missing one of "%1", "%2", "%3", "%4", "%5", "%6", "%7", "%8", "%9", "%10", "%11", "%12" start-tag';
+        
+        $this->normalFormTest($htmlErrorString, $expectedNormalForm);
+    }    
 }
--- tests/webignition/HtmlValidationErrorNormaliser/NormaliseTest/GetParametersTest.php
+++ tests/webignition/HtmlValidationErrorNormaliser/NormaliseTest/GetParametersTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace webignition\HtmlValidationErrorNormaliser\Tests\webignition\HtmlValidationErrorNormaliser\NormalisedSingleParameterErrorType;
+namespace webignition\HtmlValidationErrorNormaliser\Tests\webignition\HtmlValidationErrorNormaliser\NormaliseTest;
 
 use webignition\HtmlValidationErrorNormaliser\Tests\webignition\HtmlValidationErrorNormaliser\BaseTest;
 
@@ -58,6 +58,51 @@ class GetParametersTest extends BaseTest {
                 'tbody'
             )
         );    
-    }    
+    }  
+    
+    
+    public function testDocumentTypeDoesnotAllowElementHereMissingOneOfTwoParameters() {
+        $this->parametersTest(
+            'document type does not allow element "script" here; missing one of "dt", "dd" start-tag',
+             array(
+                 'script',
+                 'dt',
+                 'dd'
+            )
+        );
+    }
+    
+    public function testDocumentTypeDoesnotAllowElementHereMissingOneOfThreeParameters() {        
+        $this->parametersTest(
+            'document type does not allow element "DIV" here; missing one of "OBJECT", "MAP", "BUTTON" start-tag',
+             array(
+                 'DIV',
+                 'OBJECT',
+                 'MAP',
+                 'BUTTON'
+            )
+        );        
+    }
+    
+    public function testDocumentTypeDoesnotAllowElementHereMissingOneOfManyParameters() {        
+        $this->parametersTest(
+            'document type does not allow element "img" here; missing one of "p", "h1", "h2", "h3", "h4", "h5", "h6", "div", "address", "fieldset", "ins", "del" start-tag',
+             array(
+                 'img',
+                 'p',
+                 'h1',
+                 'h2',
+                 'h3',
+                 'h4',
+                 'h5',
+                 'h6',
+                 'div',
+                 'address',
+                 'fieldset',
+                 'ins',
+                 'del'
+            )
+        );        
+    }      
     
 }
