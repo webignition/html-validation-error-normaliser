e6a31f9cc5de7673a9a551e2594dcc4a5578ae09
Jon Cram <jon@webignition.net>
2013-07-22 09:49:49 +0100

    Refactor: make general solution for html5 pattern-based errors

--- src/webignition/HtmlValidationErrorNormaliser/HtmlValidationErrorNormaliser.php
+++ src/webignition/HtmlValidationErrorNormaliser/HtmlValidationErrorNormaliser.php
@@ -9,22 +9,23 @@ use webignition\HtmlValidationErrorNormaliser\NormalisedError;
  * 
  * W3C html error reference: http://validator.w3.org/docs/errors.html
  */
-class HtmlValidationErrorNormaliser { 
+class HtmlValidationErrorNormaliser {   
+    
     
     /**
      * 
      * @param string $htmlErrorString
      * @return array
      */
-    public function normalise($htmlErrorString) {
+    public function normalise($htmlErrorString) {        
         $result = new Result();
         $result->setRawError($htmlErrorString);
         
-        if (($normalisedError = $this->getNonQuotedParameterNormalisedError($htmlErrorString)) !== false) {            
+        $patternBasedNormaliser = new PatternBasedNormaliser();
+        if (($normalisedError = $patternBasedNormaliser->normalise($htmlErrorString)) !== false) {                        
             $result->setNormalisedError($normalisedError);               
-            return $result;               
+            return $result;            
         }
-        
         
         if (($normalisedError = $this->getQuotedParameterNormalisedError($htmlErrorString)) !== false) {            
             $result->setNormalisedError($normalisedError);               
@@ -35,23 +36,7 @@ class HtmlValidationErrorNormaliser {
     }
     
     
-    private function getQuotedParameterNormalisedError($htmlErrorString) {                
-        if (($normalisedError = $this->getCharacterXIsNotAllowedInTheValueOfAttributeY($htmlErrorString)) !== false) {            
-            return $normalisedError;            
-        }        
-        
-        if (($normalisedError = $this->getIdAlreadyDefinedError($htmlErrorString)) !== false) {            
-            return $normalisedError;            
-        }            
-        
-        if (($normalisedError = $this->getDocumentDoesNotAllowElementXHereMissingOneOfYError($htmlErrorString)) !== false) {            
-            return $normalisedError;            
-        }     
-        
-        if (($normalisedError = $this->getValueOfAttributeXCannotBeYMustBeOneOfZError($htmlErrorString)) !== false) {            
-            return $normalisedError;            
-        };
-        
+    private function getQuotedParameterNormalisedError($htmlErrorString) {        
         $parameterMatches = array();
         $matchCount = preg_match_all('/"([^"]?)+"/', $htmlErrorString, $parameterMatches);
         
@@ -81,190 +66,6 @@ class HtmlValidationErrorNormaliser {
         return $escapedValue;
     }
     
-    private function getIdAlreadyDefinedError($htmlErrorString) {                        
-        if (preg_match('/ID .+ already defined/i', $htmlErrorString)) {
-            $normalisedError = new NormalisedError();            
-            $normalisedError->setNormalForm('ID "%0" already defined');            
-            $normalisedError->addParameter(str_replace(array(
-                'ID "',
-                '" already defined'
-            ), '', $htmlErrorString));
-            
-            return $normalisedError;
-        }
-        
-        return false;
-    }    
-    
-    private function getValueOfAttributeXCannotBeYMustBeOneOfZError($htmlErrorString) {                
-        if (preg_match('/value of attribute ".+" cannot be ".+"; must be one of/i', $htmlErrorString)) {            
-            $primaryErrorParts = explode('; must be one of ', $htmlErrorString);            
-            $errorParts = array_merge(explode(' cannot be ', $primaryErrorParts[0]), array($primaryErrorParts[1]));
-
-            $normalisedError = new NormalisedError();            
-            $normalisedError->setNormalForm('value of attribute "%0" cannot be "%1"; must be one of %2');
-      
-            
-            $normalisedError->addParameter(trim(str_replace('value of attribute ', '', $errorParts[0]), '"'));
-            $normalisedError->addParameter(trim($errorParts[1], '"'));            
-            $normalisedError->addParameter($errorParts[2]);
-            
-            return $normalisedError;
-        }
-        
-        return false;
-    }
-    
-    private function getCharacterXIsNotAllowedInTheValueOfAttributeY($htmlErrorString) {        
-        if (preg_match('/character ".+" is not allowed in the value of attribute ".+"/i', $htmlErrorString)) {                        
-            $errorParts = explode(' is not allowed in the value of attribute ', $htmlErrorString);
-
-
-            $normalisedError = new NormalisedError();            
-            $normalisedError->setNormalForm('character "%0" is not allowed in the value of attribute "%1"');
-            
-            if (substr_count($errorParts[0], '"""')) {
-                $normalisedError->addParameter('"');
-            } else {
-                $normalisedError->addParameter(trim(str_replace('character ', '', $errorParts[0]), '"'));
-            }
-            
-            $normalisedError->addParameter(trim($errorParts[1], '"'));
-            return $normalisedError;
-        }
-        
-        return false;
-    }    
-    
-    
-    private function getDocumentDoesNotAllowElementXHereMissingOneOfYError($htmlErrorString) {        
-        if (preg_match('/document type does not allow element ".+" here; missing one of/i', $htmlErrorString)) {            
-            $errorParts = explode('; missing one of ', $htmlErrorString);
-            
-            $normalisedError = new NormalisedError();            
-            $normalisedError->setNormalForm('Document type does not allow element "%0" here; missing one of %1 start-tag');
-            
-            $normalisedError->addParameter(str_replace(array(
-                'Document type does not allow element "',
-                'document type does not allow element "',
-                '" here'
-            ), '', $errorParts[0]));
-            $normalisedError->addParameter(str_replace(' start-tag', '', $errorParts[1]));
-            
-            return $normalisedError;
-        }
-        
-        return false;
-    }    
-    
-    
-    private function getNonQuotedParameterNormalisedError($htmlErrorString) {
-        if (($normalisedError = $this->getDuplicatePropertyX($htmlErrorString)) !== false) {
-            return $normalisedError;             
-        }         
-        
-        if (($normalisedError = $this->getStrayEndTagError($htmlErrorString)) !== false) {
-            return $normalisedError;             
-        }          
-        
-        if (($normalisedError = $this->getAttributeXNotAllowedOnElementYNormalisedError($htmlErrorString)) !== false) {
-            return $normalisedError;             
-        }        
-        
-        if (($normalisedError = $this->getElementXNotAllowedAsChildOfElementYInThisContextError($htmlErrorString)) !== false) {
-            return $normalisedError;             
-        }        
-        
-        return false;
-    }
-    
-    private function getAttributeXNotAllowedOnElementYNormalisedError($htmlErrorString) {
-        if (preg_match('/Attribute .+ not allowed on element .+ at this point\./i', $htmlErrorString)) {
-            $errorParts = explode(' not allowed on element ', $htmlErrorString);
-            
-            $normalisedError = new NormalisedError();            
-            $normalisedError->setNormalForm('Attribute %0 not allowed on element %1 at this point.');
-            
-            $normalisedError->addParameter(str_replace(array('Attribute ', 'attribute '), '', $errorParts[0]));
-            $normalisedError->addParameter(str_replace(' at this point.', '', $errorParts[1]));
-            
-            return $normalisedError;
-        }        
-        
-        return false;
-    }
-    
-    private function getElementXNotAllowedAsChildOfElementYInThisContextError($htmlErrorString) {
-        if (preg_match('/Element .+ not allowed as child of element .+ in this context\./i', $htmlErrorString)) {
-            $errorParts = explode(' not allowed as child of element ', $htmlErrorString);
-            
-            $parameter0Parts = explode(' ', $errorParts[0], 2);
-            $parameter1Parts = explode(' ', $errorParts[1], 2);
-            
-            $normalisedError = new NormalisedError();            
-            $normalisedError->setNormalForm('Element %0 not allowed as child of element %1 in this context. (Suppressing further errors from this subtree.)');
-            
-            $normalisedError->addParameter($parameter0Parts[1]);
-            $normalisedError->addParameter($parameter1Parts[0]);
-            
-            return $normalisedError;
-        }        
-        
-        return false;        
-    }
-    
-    private function getStrayEndTagError($htmlErrorString) {
-        $identifier = 'Stray end tag ';
-        $prefix = 'Stray end tag ';
-        $suffix = '.';
-        
-        if (substr($htmlErrorString, 0, strlen($identifier)) !== $identifier) {
-            return false;
-        }
-        
-        $parameter = str_replace(array(
-            $prefix,
-            $suffix
-        ), '', $htmlErrorString);
-        
-        $normalisedError = new NormalisedError();            
-        $normalisedError->setNormalForm($prefix . '%0' . $suffix);
-
-        $normalisedError->addParameter($parameter);
-        return $normalisedError;
-    }
-    
-    
-    private function getDuplicatePropertyX($htmlErrorString) {
-        $parts = explode(' ', $htmlErrorString);
-        if (count($parts) !== 3) {
-            return false;
-        }
-        
-        if ($parts[0] !== 'Duplicate') {
-            return false;
-        }
-        
-        $normalisedError = new NormalisedError();            
-        $normalisedError->setNormalForm('Duplicate '. $parts[1] .' %0.');
-
-        $normalisedError->addParameter(trim($parts[2], '.'));
-        
-        return $normalisedError;
-    }
-
-    
-    // Unclosed element div.
-    // Unclosed element %0
-    // 
-    // 
-    // End tag h3 seen, but there were open elements
-    // End tag %0 seen, but there were open elements
-    // 
-    // 
-    // Bad value 100% for attribute width on element img: Expected a digit but saw % instead.
-    // Bad value %0 for attribute %1 on element %2: %3
-    //
     
     
 }
---
+++ src/webignition/HtmlValidationErrorNormaliser/PatternBasedNormaliser.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace webignition\HtmlValidationErrorNormaliser;
+
+use webignition\HtmlValidationErrorNormaliser\NormalisedError;
+
+class PatternBasedNormaliser {
+    
+    private $html5ErrorPatterns = array(
+        array(
+            'Duplicate ID ',
+            '{{ token_0 }}',
+            '.'
+        ),
+        array(
+            'Duplicate attribute ',
+            '{{ token_0 }}',
+            '.'
+        ),
+        array(
+            'Stray end tag ',
+            '{{ token_0 }}',
+            '.'
+        ),        
+        array(
+            'Attribute ',
+            '{{ token_0 }}',
+            ' not allowed on element ',
+            '{{ token_1 }}',
+            ' at this point.'
+        ),        
+        array(
+            'Element ',
+            '{{ token_0 }}',
+            ' not allowed as child of element ',
+            '{{ token_1 }}',
+            ' in this context. (Suppressing further errors from this subtree.)'
+        ),
+        array(
+            'character "',
+            '{{ token_0 }}"',
+            '" is not allowed in the value of attribute "',
+            '{{ token_1 }}',
+            '"'
+        ),
+        array(
+            'ID "',
+            '{{ token_0 }}"',
+            '" already defined'
+        ),
+        array(
+            'document type does not allow element "',
+            '{{ token_0 }}"',
+            '" here; missing one of ',
+            '{{ token_1 }}',
+            ' start-tag'
+        ),
+        array(
+            'value of attribute "',
+            '{{ token_0 }}',
+            '" cannot be "',
+            '{{ token_1 }}',
+            '"; must be one of ',
+            '{{ token_2 }}'
+        ),
+    );
+    
+    
+    /**
+     * 
+     * @param string $htmlErrorString
+     * @return NormalisedError
+     */
+    public function normalise($htmlErrorString) {
+        foreach ($this->html5ErrorPatterns as $pattern) {
+            if (($normalisedError = $this->getFromHtml5Pattern($htmlErrorString, $pattern)) !== false) {
+                return $normalisedError;             
+            }
+        }
+        
+        return false;
+    }
+    
+    private function getFromHtml5Pattern($htmlErrorString, $pattern) {                
+        if ($this->matches($htmlErrorString, $pattern)) {            
+            $matches = array();
+            
+            if (preg_match_all($this->getRegexPatternFromHtmlErrorPattern($pattern), $htmlErrorString, $matches) === 0) {
+                return false;
+            }
+            
+            foreach ($matches[0] as $matchString) {
+                $parameters = $this->getParametersFromMatchString($matchString, $pattern);
+            }
+            
+            $normalisedError = new NormalisedError();
+            $normalisedError->setNormalForm($this->getNormalForm($pattern));
+            foreach ($parameters as $parameter) {
+                $normalisedError->addParameter($parameter);
+            } 
+            
+            return $normalisedError;
+        }   
+
+        return false;
+    }
+    
+    
+    private function getNormalForm($pattern) {
+        $normalForm = '';
+        $parameterIndex = 0;
+        
+        foreach ($pattern as $part) {
+            if ($this->isTokenPatternPart($part)) {
+                $normalForm .= '%' . $parameterIndex;
+                $parameterIndex++;
+            } else {
+                $normalForm .= $part;
+            }
+        }
+        
+        return $normalForm;
+    }
+    
+    private function getParametersFromMatchString($matchString, $pattern) {        
+        $modifiedMatchString = $matchString;
+        $parameters = array();
+        
+        $patternTokenCount = $this->getPatternTokenCount($pattern);
+        $patternPartCount = count($pattern);
+        
+        foreach ($pattern as $partIndex => $part) {            
+            if ($this->isTokenPatternPart($part)) {
+                if ($partIndex == $patternPartCount - 1 && count($parameters) < $patternTokenCount) {
+                    $parameters[] = $modifiedMatchString;
+                }
+            } else {
+                $tokenSeparatedParts = explode($part, $modifiedMatchString);
+                
+                if (count($tokenSeparatedParts) === 1) {                    
+                    return $parameters;
+                }                
+                
+                if ($tokenSeparatedParts[0] !== '') {
+                    $parameters[] = $tokenSeparatedParts[0];
+                }
+                
+                $modifiedMatchString = $tokenSeparatedParts[1];                
+            }
+        }
+        
+        return $parameters;
+    }
+    
+    private function getPatternTokenCount($pattern) {
+        $tokenCount = 0;
+        
+        foreach ($pattern as $part) {
+            if ($this->isTokenPatternPart($part)) {
+                $tokenCount++;
+            }
+        }
+        
+        return $tokenCount;
+    }
+    
+    private function matches($htmlErrorString, $pattern) {        
+        return preg_match($this->getRegexPatternFromHtmlErrorPattern($pattern), $htmlErrorString) > 0;
+    }
+    
+    
+    private function getRegexPatternFromHtmlErrorPattern($pattern) {
+        $regexPattern = '';
+        
+        foreach ($pattern as $part) {
+            if ($this->isTokenPatternPart($part)) {
+                $regexPattern .= '.+';
+            } else {
+                $regexPattern .= $this->preg_quote($part);
+            }
+        }
+        
+        return '/' . $regexPattern . '/i';
+    }
+    
+    
+    private function preg_quote($value) {
+        $escapedValue = preg_quote($value);
+        if (substr_count($escapedValue, '/') && !substr_count($escapedValue, '\/')) {
+            $escapedValue = str_replace('/', '\/', $escapedValue);
+        }
+        
+        return $escapedValue;
+    }    
+    
+    private function isTokenPatternPart($part) {
+        return preg_match('/{{ token_[0-9]+ }}/', $part) > 0;
+    }
+    
+    /**
+     * 
+     */
+
+    
+    // Unclosed element div.
+    // Unclosed element %0
+    // 
+    // 
+    // End tag h3 seen, but there were open elements
+    // End tag %0 seen, but there were open elements
+    // 
+    // 
+    // Bad value 100% for attribute width on element img: Expected a digit but saw % instead.
+    // Bad value %0 for attribute %1 on element %2: %3
+    // 
+    // No p element in scope but a p end tag seen
+    // No %0 element in scope but a %0 end tag seen
+    // 
+    //
+    
+    
+    
+}
