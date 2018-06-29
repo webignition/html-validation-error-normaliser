da98600d47adc122672223afe07163c68c1e3f43
Jon Cram <jon@webignition.net>
2018-06-29 13:29:28 +0100

    Move obsolete tests

--- tests/DataProvider/QuotedParameterNormalisedFormDataProviderTrait.php
+++ tests/DataProvider/QuotedParameterNormalisedFormDataProviderTrait.php
@@ -12,13 +12,39 @@ trait QuotedParameterNormalisedFormDataProviderTrait
     public function normaliseHasQuotedParameterNormalisedFormDataProvider()
     {
         return [
-
             'general entity "%0" not defined and no default entity' => [
                 'htmlErrorString' => 'general entity "t" not defined and no default entity',
                 'expectedNormalisedError' => new NormalisedError(
                     'general entity "%0" not defined and no default entity',
                     [
                         't',
+                    ]
+                ),
+            ],
+            'unknown declaration type "%0"' => [
+                'htmlErrorString' => 'unknown declaration type "doctype"',
+                'expectedNormalisedError' => new NormalisedError(
+                    'unknown declaration type "%0"',
+                    [
+                        'doctype',
+                    ]
+                ),
+            ],
+            'document type does not allow element "%0" here' => [
+                'htmlErrorString' => 'document type does not allow element "style" here',
+                'expectedNormalisedError' => new NormalisedError(
+                    'document type does not allow element "%0" here',
+                    [
+                        'style',
+                    ]
+                ),
+            ],
+            'end tag for "%0" omitted, but its declaration does not permit this' => [
+                'htmlErrorString' => 'end tag for "FONT" omitted, but its declaration does not permit this',
+                'expectedNormalisedError' => new NormalisedError(
+                    'end tag for "%0" omitted, but its declaration does not permit this',
+                    [
+                        'FONT',
                     ]
                 ),
             ],
