fcb799ca992ffb00f7a7c24699d66982f5a8d1d8
Jon Cram <jon@webignition.net>
2018-06-29 12:54:30 +0100

    stash

--- tests/DataProvider/PatternBasedNormalisedFormDataProviderTrait.php
+++ tests/DataProvider/PatternBasedNormalisedFormDataProviderTrait.php
@@ -2,80 +2,14 @@
 
 namespace webignition\HtmlValidationErrorNormaliser\Tests\webignition\HtmlValidationErrorNormaliser;
 
-use webignition\HtmlValidationErrorNormaliser\HtmlValidationErrorNormaliser;
 use webignition\HtmlValidationErrorNormaliser\NormalisedError;
-use webignition\HtmlValidationErrorNormaliser\PatternBasedNormaliser;
-use webignition\HtmlValidationErrorNormaliser\Result;
 
-class HtmlValidationErrorNormaliserTest extends \PHPUnit_Framework_TestCase
+trait PatternBasedNormalisedFormDataProviderTrait
 {
     /**
-     * @var HtmlValidationErrorNormaliser
-     */
-    private $htmlValidationErrorNormaliser;
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function setUp()
-    {
-        parent::setUp();
-
-        $this->htmlValidationErrorNormaliser = new HtmlValidationErrorNormaliser();
-    }
-
-    /**
-     * @dataProvider normaliseHasNoNormalisedFormDataProvider
-     *
-     * @param string $htmlErrorString
-     */
-    public function testNormaliseHasNoNormalisedForm($htmlErrorString)
-    {
-        $result = $this->htmlValidationErrorNormaliser->normalise($htmlErrorString);
-
-        $this->assertInstanceOf(Result::class, $result);
-        $this->assertFalse($result->isNormalised());
-        $this->assertNull($result->getNormalisedError());
-    }
-
-    /**
      * @return array
      */
-    public function normaliseHasNoNormalisedFormDataProvider()
-    {
-        return [
-            'no normalisation needed [0]' => [
-                'htmlErrorString' => 'unterminated comment: found end of entity inside comment',
-            ],
-            'no normalisation needed [1]' => [
-                'htmlErrorString' => 'literal is missing closing delimiter',
-            ],
-            'no normalisation needed [2]' => [
-                'htmlErrorString' => 'character data is not allowed here',
-            ],
-        ];
-    }
-
-    /**
-     * @dataProvider normaliseHasNormalisedFormDataProvider
-     *
-     * @param string $htmlErrorString
-     * @param NormalisedError $expectedNormalisedError
-     */
-    public function testNormaliseHasNormalisedForm($htmlErrorString, NormalisedError $expectedNormalisedError)
-    {
-        $result = $this->htmlValidationErrorNormaliser->normalise($htmlErrorString);
-
-        $this->assertInstanceOf(Result::class, $result);
-        $this->assertTrue($result->isNormalised());
-
-        $this->assertEquals($expectedNormalisedError, $result->getNormalisedError());
-    }
-
-    /**
-     * @return array
-     */
-    public function normaliseHasNormalisedFormDataProvider()
+    public function normaliseHasPatternBasedNormalisedFormDataProvider()
     {
         return [
             'Attribute %0 not allowed on element %1 at this point. [0]' => [
