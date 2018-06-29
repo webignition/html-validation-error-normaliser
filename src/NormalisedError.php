bc27220fe2df505a5e8247f2075afa69b4c5324a
Jon Cram <jon@webignition.net>
2018-06-29 07:51:11 +0100

    PSR2 NormalisedError, re-implement test

--- src/NormalisedError.php
+++ src/NormalisedError.php
@@ -2,64 +2,53 @@
 
 namespace webignition\HtmlValidationErrorNormaliser;
 
-use webignition\HtmlValidationErrorNormaliser\ErrorType\ErrorType;
-use webignition\HtmlValidationErrorNormaliser\ErrorType\Normaliser;
-
-
 /**
  * Stores a normalised HTML validation error
  */
-class NormalisedError {
-    
+class NormalisedError
+{
     /**
-     * 
      * @var string
      */
     private $normalForm;
-    
-    
+
     /**
      * Collection of parameters to place into the normal form
      * placeholders to generate the non-normal form.
-     * 
+     *
      * @var array
      */
-    private $parameters = array();
-    
-    
+    private $parameters = [];
+
     /**
-     * 
      * @param string $normalForm
      */
-    public function setNormalForm($normalForm) {
+    public function setNormalForm($normalForm)
+    {
         $this->normalForm = $normalForm;
     }
-    
-    
+
     /**
-     * 
      * @return string
      */
-    public function getNormalForm() {
+    public function getNormalForm()
+    {
         return $this->normalForm;
     }
-    
-    
+
     /**
-     * 
      * @param string $value
      */
-    public function addParameter($value) {
+    public function addParameter($value)
+    {
         $this->parameters[] = $value;
     }
-    
-    
+
     /**
-     * 
      * @return array
      */
-    public function getParameters() {
+    public function getParameters()
+    {
         return $this->parameters;
     }
-    
 }
---
+++ tests/NormalisedErrorTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace webignition\HtmlValidationErrorNormaliser\Tests\webignition\HtmlValidationErrorNormaliser;
+
+use webignition\HtmlValidationErrorNormaliser\NormalisedError;
+
+class NormalisedErrorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var NormalisedError
+     */
+    private $normalisedError;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->normalisedError = new NormalisedError();
+    }
+
+    public function testGetNormalFormSetNormalForm()
+    {
+        $normalForm = 'foo';
+
+        $this->assertNull($this->normalisedError->getNormalForm());
+
+        $this->normalisedError->setNormalForm($normalForm);
+        $this->assertEquals($normalForm, $this->normalisedError->getNormalForm());
+    }
+
+    /**
+     * @dataProvider addParametersGetParametersDataProvider
+     *
+     * @param array $parameters
+     */
+    public function testAddParameterGetParameters(array $parameters)
+    {
+        foreach ($parameters as $parameter) {
+            $this->normalisedError->addParameter($parameter);
+        }
+
+        $this->assertEquals($parameters, $this->normalisedError->getParameters());
+    }
+
+    /**
+     * @return array
+     */
+    public function addParametersGetParametersDataProvider()
+    {
+        return [
+            'none' => [
+                'parameters' => [],
+            ],
+            'non-empty' => [
+                'parameters' => [
+                    'foo',
+                    'bar',
+                ],
+            ],
+        ];
+    }
+}
