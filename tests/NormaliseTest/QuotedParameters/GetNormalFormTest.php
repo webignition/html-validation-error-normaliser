4b7bf7ac6e936d44be2be89dd7986b563756ceb4
Jon Cram <jon@webignition.net>
2018-06-29 13:48:25 +0100

    Remove obsolete tests

--- tests/NormaliseTest/QuotedParameters/GetNormalFormTest.php
+++ tests/NormaliseTest/QuotedParameters/GetNormalFormTest.php
@@ -6,34 +6,6 @@ use webignition\HtmlValidationErrorNormaliser\Tests\webignition\HtmlValidationEr
 
 class GetNormalFormTest extends BaseTest {
 
-    public function testIdAltreadyDefinedOne() {
-        $htmlErrorString = 'ID "\"playerbox_"+playerid+"\"" already defined';
-        $expectedNormalForm = 'ID "%0" already defined';
-
-        $this->normalFormTest($htmlErrorString, $expectedNormalForm);
-    }
-
-    public function testIdAltreadyDefinedTwo() {
-        $htmlErrorString = 'ID "\"contenedor_ayuda\"" already defined';
-        $expectedNormalForm = 'ID "%0" already defined';
-
-        $this->normalFormTest($htmlErrorString, $expectedNormalForm);
-    }
-
-    public function testCharacterBlankIsNotAllowedInTheValueOfAttributeXOne() {
-        $htmlErrorString = 'character """ is not allowed in the value of attribute "id"';
-        $expectedNormalForm = 'character "%0" is not allowed in the value of attribute "%1"';
-
-        $this->normalFormTest($htmlErrorString, $expectedNormalForm);
-    }
-
-    public function testCharacterBlankIsNotAllowedInTheValueOfAttributeXTwo() {
-        $htmlErrorString = 'character "a" is not allowed in the value of attribute "foo"';
-        $expectedNormalForm = 'character "%0" is not allowed in the value of attribute "%1"';
-
-        $this->normalFormTest($htmlErrorString, $expectedNormalForm);
-    }
-
     public function testElementXUndefined() {
         $htmlErrorString = 'Element "Variable" undefined';
         $expectedNormalForm = 'Element "%0" undefined';
