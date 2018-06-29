da7e8eb34a3eac5ed36bfcc0d69fa5f35a594b8c
Jon Cram <jon@webignition.net>
2018-06-29 12:42:34 +0100

    Add cases for no normalisation needed to PatternBasedNormaliser

--- tests/PatternBasedNormaliserTest.php
+++ tests/PatternBasedNormaliserTest.php
@@ -41,6 +41,15 @@ class PatternBasedNormaliserTest extends \PHPUnit_Framework_TestCase
             'empty' => [
                 'htmlErrorString' => '',
             ],
+            'no normalisation needed [0]' => [
+                'htmlErrorString' => 'unterminated comment: found end of entity inside comment',
+            ],
+            'no normalisation needed [1]' => [
+                'htmlErrorString' => 'literal is missing closing delimiter',
+            ],
+            'no normalisation needed [2]' => [
+                'htmlErrorString' => 'character data is not allowed here',
+            ],
         ];
     }
 
