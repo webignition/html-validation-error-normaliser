f54e61e8d4ba28db468ebc0baed45f3164d349f3
Jon Cram <jon@webignition.net>
2018-06-29 13:33:49 +0100

    Remove obsolete tests

--- tests/NormaliseTest/QuotedParameters/GetNormalFormTest.php
+++ tests/NormaliseTest/QuotedParameters/GetNormalFormTest.php
@@ -6,27 +6,6 @@ use webignition\HtmlValidationErrorNormaliser\Tests\webignition\HtmlValidationEr
 
 class GetNormalFormTest extends BaseTest {
 
-    public function testDocumentTypeDoesnotAllowElementHereMissingOneOfTwoParameters() {
-        $htmlErrorString = 'document type does not allow element "script" here; missing one of "dt", "dd" start-tag';
-        $expectedNormalForm = 'document type does not allow element "%0" here; missing one of %1 start-tag';
-
-        $this->normalFormTest($htmlErrorString, $expectedNormalForm);
-    }
-
-    public function testDocumentTypeDoesnotAllowElementHereMissingOneOfThreeParameters() {
-        $htmlErrorString = 'document type does not allow element "DIV" here; missing one of "OBJECT", "MAP", "BUTTON" start-tag';
-        $expectedNormalForm = 'document type does not allow element "%0" here; missing one of %1 start-tag';
-
-        $this->normalFormTest($htmlErrorString, $expectedNormalForm);
-    }
-
-    public function testDocumentTypeDoesnotAllowElementHereMissingOneOfManyParameters() {
-        $htmlErrorString = 'document type does not allow element "img" here; missing one of "p", "h1", "h2", "h3", "h4", "h5", "h6", "div", "address", "fieldset", "ins", "del" start-tag';
-        $expectedNormalForm = 'document type does not allow element "%0" here; missing one of %1 start-tag';
-
-        $this->normalFormTest($htmlErrorString, $expectedNormalForm);
-    }
-
     public function testValueOfAttributeXCannotBeYMustBeOneOfZ() {
         $htmlErrorString = 'value of attribute "type" cannot be "email"; must be one of "text", "password", "checkbox", "radio", "submit", "reset", "file", "hidden", "image", "button"';
         $expectedNormalForm = 'value of attribute "%0" cannot be "%1"; must be one of %2';
